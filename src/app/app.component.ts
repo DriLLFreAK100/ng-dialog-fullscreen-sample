62aa3f488748f88f065bdfa2a50534c9659e75ff
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-05-01 23:10:03 +0800

    fullscreen dialog sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
+import { SampleDialogComponent } from './components/sample-dialog/sample-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-fullscreen-dialog-sample';
+  @ViewChild('fullscreenArea') fullscreenArea!: ElementRef;
+  isFullscreen$ = new BehaviorSubject<boolean>(false);
+
+  constructor(public dialog: MatDialog) {}
+
+  public handleOpenDialog() {
+    this.dialog.open(SampleDialogComponent);
+  }
+
+  public handleClickFullscreen() {
+    if (!this.isFullscreen$.value) {
+      this.fullscreenArea.nativeElement.requestFullscreen();
+      return;
+    }
+
+    document.exitFullscreen();
+  }
+
+  @HostListener('document:fullscreenchange', ['$event'])
+  @HostListener('document:webkitfullscreenchange', ['$event'])
+  @HostListener('document:mozfullscreenchange', ['$event'])
+  @HostListener('document:MSFullscreenChange', ['$event'])
+  public fullscreenEvent() {
+    if (document.fullscreenElement) {
+      this.isFullscreen$.next(true);
+      return;
+    }
+
+    this.isFullscreen$.next(false);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { SampleDialogComponent } from './components/sample-dialog/sample-dialog.component';
+import {
+  FullscreenOverlayContainer,
+  OverlayContainer,
+} from '@angular/cdk/overlay';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SampleDialogComponent],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: OverlayContainer, useClass: FullscreenOverlayContainer },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
